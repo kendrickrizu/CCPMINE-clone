284634dea0d8f894718e1b90bb82d235d7731080
KENECHUKWU AMARAIZU <kendrickrizu@gmail.com>
2025-10-05 00:16:39 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,122 @@
+export const activities = [
+  {
+    icon: "fa-solid fa-user-plus",
+    title: "Invite"
+  },
+  {
+    icon: "fa-solid fa-server",
+    title: "Miner"
+  },
+  {
+    icon: "fa-solid fa-gift",
+    title: "Bonus"
+  },
+  {
+    icon: "fa-solid fa-arrows-rotate",
+    title: "Exchange"
+  },
+  {
+    icon: "fa-solid fa-ticket",
+    title: "Raffle"
+  },
+  {
+    icon: "fa-solid fa-tags",
+    title: "Coupon"
+  },
+  {
+    icon: "fa-solid fa-circle-info",
+    title: "About Us"
+  },
+  {
+    icon: "fa-solid fa-building",
+    title: "Company"
+  }
+];
+
+
+export const coins = [
+  {
+    name: "BTC",
+    value: 27850.12,
+    percentageChange: "+1.24%"
+  },
+  {
+    name: "ETH",
+    value: 1820.65,
+    percentageChange: "-0.87%"
+  },
+  {
+    name: "CCP",
+    value: 0.065,
+    percentageChange: "+2.15%"
+  },
+  {
+    name: "BNB",
+    value: 215.45,
+    percentageChange: "+0.45%"
+  },
+  {
+    name: "SOL",
+    value: 24.33,
+    percentageChange: "-1.12%"
+  },
+  {
+    name: "XRP",
+    value: 0.51,
+    percentageChange: "+0.92%"
+  },
+  {
+    name: "ADA",
+    value: 0.27,
+    percentageChange: "-0.35%"
+  },
+  {
+    name: "DOGE",
+    value: 65.30,
+    percentageChange: "+1.08%"
+  }
+];
+
+
+export const machines = [
+  {
+    image: "images/$180machine.png",
+    title: "BTC cloud computing power M5",
+    netIncomePerDay: "$7",
+    totalRevenue: "$980",
+    purchaseLimit: 1,
+    validityPeriod: "140 days",
+    computingPower: 200,
+    cost: "180 USD"
+  },
+  {
+    image: "images/$400machine.png",
+    title: "BTC cloud computing power M6",
+    netIncomePerDay: "$12",
+    totalRevenue: "$2400",
+    purchaseLimit: 1,
+    validityPeriod: "200 days",
+    computingPower: 240,
+    cost: "400 USD"
+  },
+  {
+    image: "images/$80machine.png",
+    title: "BTC cloud computing power M4",
+    netIncomePerDay: "$4",
+    totalRevenue: "$320",
+    purchaseLimit: 1,
+    validityPeriod: "80 days",
+    computingPower: 160,
+    cost: "80 USD"
+  },
+  {
+    image: "images/$10machine.png",
+    title: "BTC cloud computing power M1",
+    netIncomePerDay: 1,
+    totalRevenue: "$20",
+    purchaseLimit: 1,
+    validityPeriod: "20 days",
+    computingPower: 100,
+    cost: "10 USD"
+  }
+];
--- index.js
+++ index.js
@@ -1,127 +1,7 @@
 const machineSection = document.querySelector(".machine-section")
 const cryptoCurrencies = document.querySelector(".crypto-currencies")
 const activitiesSection = document.querySelector(".activity-cards")
-
-const activities = [
-  {
-    icon: "fa-solid fa-user-plus",
-    title: "Invite"
-  },
-  {
-    icon: "fa-solid fa-server",
-    title: "Miner"
-  },
-  {
-    icon: "fa-solid fa-gift",
-    title: "Bonus"
-  },
-  {
-    icon: "fa-solid fa-arrows-rotate",
-    title: "Exchange"
-  },
-  {
-    icon: "fa-solid fa-ticket",
-    title: "Raffle"
-  },
-  {
-    icon: "fa-solid fa-tags",
-    title: "Coupon"
-  },
-  {
-    icon: "fa-solid fa-circle-info",
-    title: "About Us"
-  },
-  {
-    icon: "fa-solid fa-building",
-    title: "Company"
-  }
-];
-
-
-const coins = [
-  {
-    icon: "🪙",
-    name: "BTC",
-    value: 27850.12,
-    percentageChange: "+1.24%"
-  },
-  {
-    icon: "🪙",
-    name: "ETH",
-    value: 1820.65,
-    percentageChange: "-0.87%"
-  },
-  {
-    icon: "🪙",
-    name: "DOGE",
-    value: 0.065,
-    percentageChange: "+2.15%"
-  },
-  {
-    icon: "🪙",
-    name: "BNB",
-    value: 215.45,
-    percentageChange: "+0.45%"
-  },
-  {
-    icon: "🪙",
-    name: "SOL",
-    value: 24.33,
-    percentageChange: "-1.12%"
-  },
-  {
-    icon: "🪙",
-    name: "XRP",
-    value: 0.51,
-    percentageChange: "+0.92%"
-  },
-  {
-    icon: "🪙",
-    name: "ADA",
-    value: 0.27,
-    percentageChange: "-0.35%"
-  },
-  {
-    icon: "🪙",
-    name: "LTC",
-    value: 65.30,
-    percentageChange: "+1.08%"
-  }
-];
-
-
-const machines = [
-  {
-    image: "logo.png",
-    title: "BTC cloud computing power m6",
-    netIncomePerDay: 12,
-    totalRevenue: 2400,
-    purchaseLimit: 1,
-    validityPeriod: "200 days",
-    computingPower: 240,
-    cost: "400 USD"
-  },
-  {
-    image: "eth-logo.png",
-    title: "ETH cloud computing power e3",
-    netIncomePerDay: 8,
-    totalRevenue: 1600,
-    purchaseLimit: 2,
-    validityPeriod: "180 days",
-    computingPower: 180,
-    cost: "300 USD"
-  },
-  {
-    image: "ltc-logo.png",
-    title: "LTC cloud computing power l9",
-    netIncomePerDay: 5,
-    totalRevenue: 1000,
-    purchaseLimit: 3,
-    validityPeriod: "150 days",
-    computingPower: 120,
-    cost: "200 USD"
-  }
-];
+import { activities, coins, machines } from "/data.js";
 
 
 renderMachines()
@@ -147,7 +27,10 @@ function renderCurrencies() {
 for (const coin of coins) {
     cryptoHtml +=
             `<div class="coin-card">
-            <span class="coin-icon">${coin.icon} <span class="coin-name">${coin.name}</span></span>
+            <span class="coin-icon">
+              <img src = "images/${coin.name}.png" alt="${coin.name}">
+              <span class="coin-name">${coin.name}</span>
+            </span>
             <span class="coin-value">${coin.value}<span class="percentage-change">${coin.percentageChange}</span></span>
            </div>`
 }
@@ -162,7 +45,7 @@ for (const machine of machines) {
             `<div class="machine">
                 <span>HOT</span>
                 <div class="machine-image">
-                    <img src="logo.png"  alt="machine-image">
+                    <img src= ${machine.image}  alt="machine-image">
                 </div>
                 <div class="machine-info">
                     <h3>${machine.title}</h3>
@@ -219,7 +102,4 @@ function renderTimer() {
    timerBox.innerHTML = timerHtml
 }
 }
-
-
-
 
